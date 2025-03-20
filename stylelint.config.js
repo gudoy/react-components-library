ee9277870476fb1f15b638d3e9ee698949cc0a39
Guyllaume Doyer <doyer.guyllaume@gmail.com>
2025-03-20 12:41:52 +0100

    chore: fixed style linting issues

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,13 @@
 /** @type {import('stylelint').Config} */
+
 export default {
-  extends: ["stylelint-config-standard-scss"]
+  extends: ['stylelint-config-standard-scss'],
+  rules: {
+    'block-no-empty': null,
+    'selector-pseudo-element-colon-notation': 'double',
+    'comment-no-empty': null,
+    'scss/comment-no-empty': null,
+    'scss/double-slash-comment-empty-line-before': null,
+    'scss/double-slash-comment-whitespace-inside': null,
+  },
 };
--- stylelint.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('stylelint').Config} */
-export default {
-  extends: ['stylelint-config-standard-scss'],
-  rules: {
-    'block-no-empty': null,
-    'scss/double-slash-comment-empty-line-before': null,
-  },
-};
